b66ae7288441ac4339e7f5171f2b155f4bb130ef
Claudio <claudiosilva.cfls@gmail.com>
2018-08-11 17:13:41 -0300

    best result until now

--- server.js
+++ server.js
@@ -43,52 +43,70 @@ var scrapper = function(ticker){
 
 // Escolhendo no metodo .get() o caminho para fazer a requisição
 // Poderia ser somente a barra, mas para facilitar a compreensão vamos personalizar
-app.get('/raspagem', function(req, res) {
-    scrapper();
-    texto = "realizando";
-    res.send("está realizando a busca");    
-})
+// app.get('/raspagem', function(req, res) {
+//     scrapper();
+//     texto = "realizando";
+//     res.send("está realizando a busca");    
+// })
 
-app.get('/recebe', function(req, res) {
-    // scrapper();
-    // texto = "realizando";
-    if(data.length != 0){
-        res.json(data);
-    } else {
-        res.json([{'status': 300}]);    
-    }
-})
+// app.get('/recebe', function(req, res) {
+//     // scrapper();
+//     // texto = "realizando";
+//     if(data.length != 0){
+//         res.json(data);
+//     } else {
+//         res.json([{'status': 300}]);    
+//     }
+// })
 
-app.get('/apaga', function(req, res) {
-    // scrapper();
-    // texto = "realizando";
-    data = [];
-    res.redirect('/');
-})
+// app.get('/apaga', function(req, res) {
+//     // scrapper();
+//     // texto = "realizando";
+//     data = [];
+//     res.redirect('/');
+// })
 
 const scrapper_acoes = require('./scrape_acoes.js');
+var updateFII = function(){
+    fii_is_running = true;
+    scrapper_fii().then(
+            (value) => {
+                // res.json(value);
+                data_fii = value;
+                fii_is_running = false;
+            }
+    ).catch(
+    function(error){
+        console.log(error);
+    });
+};
+
+var updateACOES = function(){
+    acoes_is_running = true;
+    scrapper_acoes().then(
+            (value) => {
+                // res.json(value);
+                data_acoes = value;
+                acoes_is_running = false;
+            }
+    ).catch(
+    function(error){
+        console.log(error);
+    });
+            
+}
 
 app.get('/infomoney/acoes', function(req,res){
     if (data_acoes.length != 0){
         res.json(data_acoes);
     } else {
         if(!acoes_is_running){
-            acoes_is_running = true;
-            scrapper_acoes().then(
-                    (value) => {
-                        // res.json(value);
-                        data_acoes = value;
-                        acoes_is_running = false;
-                    }
-            ).catch(
-            function(error){
-                console.log(error);
-            });
-            
+            updateACOES();
         }
         res.json([{status: "300", message: "obtendo dados"}]);
     }
 })
+
 
 const scrapper_fii = require('./scrape_fii.js');
 
@@ -97,51 +115,41 @@ app.get('/infomoney/fii', function(req,res){
         res.json(data_fii);
     } else {
         if (!fii_is_running){
-            fii_is_running = true;
-            scrapper_fii().then(
-                    (value) => {
-                        // res.json(value);
-                        data_fii = value;
-                        fii_is_running = false;
-                    }
-            ).catch(
-            function(error){
-                console.log(error);
-            });
+            updateFII();
         }
         res.json([{status: "300", message: "obtendo dados"}]);
     }
 })
 
-app.get('/verifica', function(req, res) {
-    // var urls = ['ITSA4','BOVA11','ABCP11','MGLU3','PETR3','SNSL3'];
-    var entrada = {body:{ticker:'ITSA4'}};
-    // ativos.create(req,res);
-    models.Ativos.create({
-        ticker: 'ITSA4',
-        price: 12.01
-      }).then(function(user) {
-        res.send("abriu");
-      });
-    // res.send("abriu");
+// app.get('/verifica', function(req, res) {
+//     // var urls = ['ITSA4','BOVA11','ABCP11','MGLU3','PETR3','SNSL3'];
+//     var entrada = {body:{ticker:'ITSA4'}};
+//     // ativos.create(req,res);
+//     models.Ativos.create({
+//         ticker: 'ITSA4',
+//         price: 12.01
+//       }).then(function(user) {
+//         res.send("abriu");
+//       });
+//     // res.send("abriu");
     
-})
+// })
 
-app.get('/verifica2', function(req, res) {
-    // var urls = ['ITSA4','BOVA11','ABCP11','MGLU3','PETR3','SNSL3'];
-    // var entrada = {body:{ticker:'ITSA4'}};
-    // // ativos.create(req,res);
-    // models.Ativos.create({
-    //     sticker: 'ITSA4',
-    //     price: 12.01
-    //   }).then(function(user) {
-    //     res.send("abriu");
-    //   });
-      models.Ativos.findAll({}).then(function(todos) {
-        res.json(todos);
-    });
-    // res.send("abriu");
-})
+// app.get('/verifica2', function(req, res) {
+//     // var urls = ['ITSA4','BOVA11','ABCP11','MGLU3','PETR3','SNSL3'];
+//     // var entrada = {body:{ticker:'ITSA4'}};
+//     // // ativos.create(req,res);
+//     // models.Ativos.create({
+//     //     sticker: 'ITSA4',
+//     //     price: 12.01
+//     //   }).then(function(user) {
+//     //     res.send("abriu");
+//     //   });
+//       models.Ativos.findAll({}).then(function(todos) {
+//         res.json(todos);
+//     });
+//     // res.send("abriu");
+// })
 
 // var teste = function() {
 //     setTimeout(function(){
@@ -176,27 +184,10 @@ app.get('/envia', function(req, res) {
 })
 
 app.get('/', function(req, res) {
-    // var urls = ['ITSA4','BOVA11','ABCP11','MGLU3','PETR3','SNSL3'];
-    // var urls = [req.query.ticker];
-    // // console.log(req.query.ticker);
-    // var total = urls.length;
-    // var data = [];
-    // for (var i = urls.length - 1; i >= 0; i--) {
-    //     scrape(urls[i]).then(
-    //         (value) => {
-    //             data.push(value[0]);
-    //             total = total - 1;
-    //             if(total == 0){
-    //                 res.json(data);
-    //             }
-    //         }
-    //     );
-    // }
-
-    // scrapper(req.query.ticker);
-
+    updateACOES();
+    updateFII();
     // teste();
-    res.send("<p> /envia/?ticker=bova11 </p><p> /recebe </p>");
+    res.send("<h2> Info disponível :</h2> <p> /acoes </p><p> /fii </p><p></p>");
     
 })
 
